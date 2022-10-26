c2b1e4b9e62b0c183b606e2595228888359ce3a1
PatrickKolo <patrick.kolodziej@outlook.de>
2022-10-26 17:36:20 +0200

    finished Code

--- script.js
+++ script.js
@@ -58,21 +58,117 @@ let questions = [
 ];
 
 let currentQuestion = 0;
+let rightQuestions = 0;
+
+let AUDIO_SUCCESS = new Audio('sounds/success.mp3');
+let AUDIO_FAIL = new Audio('sounds/wrong.mp3');
+
+
 
 function init() {
 
-    document.getElementById('NumberOfQuestions').innerHTML = questions.length;
-    
     showQuestion();
 
 }
 
 function showQuestion() {
+
+
+    if (gameIsOver()) {                                 // Wenn die letzte Frage erreicht wurde soll der Endscreen eingeblendet werden
+        showEndScreen();
+    } else { // Show question
+        updateProgressBar();
+        updateToNextQuestion();
+    }
+}
+
+
+function gameIsOver() {
+    return currentQuestion >= questions.length
+}
+
+
+function answer(selection) {                                          // Funktion für Antwortselektion onclick funktion. Übergeben wird der jeweilige Answer button und die Variable selection
     let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);             // von dem String answer_1 wird die letzte Stelle genommen also 1
+
+    let idOfRightAnswer = 'answer_3';
+
+    if (selectedQuestionNumber == question['right_answer']) {             // richtige Frage beantwortet   letzte Stelle des Strings wird mit dem Wert der richtigen Antwort abgeglichen
+        document.getElementById(selection).parentNode.classList.add('bg-success'); // Falls Antwort richtig Button grün anzeigen
+        document.getElementById(selection).innerHTML = 'RICHTIG'; // Es wirt das Wort "RICHTIG" angeszeigt in der richtigen Antwort
+        AUDIO_SUCCESS.play();                                       // spielt Sound ab wenn richtige Antwort gegeben wurde --> Sound wurde in globale Variable oben gepackt
+
+        rightQuestions++;                                       // Counter für richtig beantwortete Fragen
+
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();                                  // spielt Sound ab wenn falsche Antwort gegeben wurde --> Sound wurde in globale Variable oben gepackt
+    }
+    document.getElementById('next-button').disabled = false;
+
+}
 
 
-    document.getElementById('questionText').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
+
+function nextQuestion(selection) {
+    currentQuestion++;                                      // zum Beispiel von 0 auf 1
+    document.getElementById('next-button').disabled = true;   // der Button wird deaktiiert und nur aktiviert wenn eine Antwort angeklicjt wurde
+    resetAnswerButtons()
+    showQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');   // Alle verfärbungen der Buttons werden entfernt
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+
+function restartGame() {
+
+    document.getElementById('quizImg').src = 'img/maze.png'; // Ursprüngliches Bild wieder anziegen
+
+    document.getElementById('cardBody').style = '';   // cardbody wieder anzeigen
+    document.getElementById('endscreen').style = 'display: none;';  // Endscreen ausblenden
+
+    currentQuestion = 0; // aktuelle Frage wieder auf Anfang setzen     
+    rightQuestions = 0; // Counter für richtige Frage wieder auf 0 setzen
+    init();  // Wieder von vorne starten
+
+
+}
+
+function showEndScreen() {
+
+    document.getElementById('endscreen').style = '';                                // Endscreen erst einmal leeren
+    document.getElementById('cardBody').style = 'display: none;';                   // Fragen und Antworte beim Endscreen ausblenden
+
+    document.getElementById('AmountOfQuestions').innerHTML = questions.length;            // Anzahl der gesamten Fragen auf dem Endscreen anzeigen
+    document.getElementById('number-of-correct-answers').innerHTML = rightQuestions;         // Anzahl der richtig beantworteren Fragen anzeigen
+
+    document.getElementById('quizImg').src = 'img/thumb.jpg';                       // Bild tauschen für den Endscreen
+}
+
+function updateToNextQuestion() {
+
+
+
+
+
+    let question = questions[currentQuestion]; // Aktuelle Frage aus dem Array an der Stelle 0
+
+    document.getElementById('NumberOfQuestions').innerHTML = questions.length;  // Anzahl der gesamten Fragen auf dem Endscreen anzeigen
+    document.getElementById('currentQuestion').innerHTML = currentQuestion + 1; // Nummer der aktuellen Frage anzeigen mit der global definierten Variable current Question
+
+    document.getElementById('questionText').innerHTML = question['question'];  // Frage wird angeziegt bzw. aus dem Array gezogen
+    document.getElementById('answer_1').innerHTML = question['answer_1'];      // Antworten werden angezeigt bzw. aus dem Array gezogen
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
@@ -80,25 +176,12 @@ function showQuestion() {
 
 }
 
-function answer(selection){
-    let question = questions[currentQuestion];
-    console.log(`Selected answer is`, selection);
-    let selectedQuestionNumber = selection.slice(-1);
-    console.log(`selectedQuestionNumber is`, selectedQuestionNumber);
-    console.log(`Current question is `, question['right_answer']);
+function updateProgressBar() {
+    let percent = (currentQuestion + 1) / questions.length;  // progressbar: Prozent ausrechnen Frage duch alle Fragen gleich fortschritt
+    percent = Math.round(percent * 100); // Zahlen runden und auf prozent Format bringen
 
-
-    let idOfRightAnswer = 'answer_3';
-
-    if (selectedQuestionNumber == question['right_answer']) {
-        document.getElementById(selection).parentNode.classList.add('bg-success');
-        document.getElementById(selection).innerHTML = 'RICHTIG';
-        document.getElementById('quizImg').innerHTML = /*html*/`<img src="img/thumb.jpg">`;
-        
-    } else {
-        document.getElementById(selection).parentNode.classList.add('bg-danger');
-        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
-    }
-    document.getElementById('next-button').disabled = false;
+    document.getElementById('progress-bar').innerHTML = `${percent}%`; // 
+    document.getElementById('progress-bar').style = `width:${percent}%`
 
 }
+
